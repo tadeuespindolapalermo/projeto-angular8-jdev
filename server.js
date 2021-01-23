0de07e356c1abb004e4b7bc2eb2ae32fc247f19f
Tadeu Espíndola Palermo <tepalermo@stefanini.com>
2021-01-23 00:18:55 -0300

    aaa

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
-app.use(express.static(__dirname + '/dist/CURSO_ANGULAR_REST'));
+app.use(express.static(__dirname + '/dist/Curso-Angular-REST'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/CURSO_ANGULAR_REST/index.html'));
+res.sendFile(path.join(__dirname+'/dist/Curso-Angular-REST/index.html'));
 });
 
 // Inicia a aplicação pela porta configurada
